1179006f27e1108ef4c10525da50987d21f97713
Jordan Janszen <jordanjanszen@hotmail.com>
2024-04-17 11:37:59 +1000

    URLs updated

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('calendar/', views.calendar_index, name='index'),
-    path('activities/', views.activities_index, name='index'),
+    path('calendar/', views.calendar_index, name='calendar_index'),
+    path('activities/', views.activities_index, name='activities_index'),
     path('activities/<int:activity_id>/', views.activities_detail, name='activities_detail'),
-    path('sleep/', views.sleep_index, name='index'),
+    path('sleep/', views.sleep_index, name='sleep_index'),
     path('sleep/<int:sleep_id>/', views.sleep_detail, name='sleep_detail'),
-    path('dashboard', views.dashboard_index, name='index'),
+    path('dashboard', views.dashboard_index, name='dashboard_index'),
 ]
